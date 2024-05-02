4c46cf38dd9063e0fa122abf02fb8fc48136c9d1
Steven Harman <steven@harmanly.com>
2024-05-02 17:56:40 -0400

    Add L2Met source attribute
    It will pull from the ENV `L2MET_SOURCE` and then `DYNO`, falling back
    to building a string based on the current Host name and executing
    program name.

--- lib/puma/plugin/telemetry/transforms/l2met_transform.rb
+++ lib/puma/plugin/telemetry/transforms/l2met_transform.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'English'
+require 'pathname'
+
 module Puma
   class Plugin
     module Telemetry
@@ -7,10 +10,34 @@ module Puma
         # L2Met (Logs to Metrics) transform that makes all keys a `sample#` in the L2Met format.
         class L2metTransform
           def self.call(telemetry)
+            new.call(telemetry)
+          end
+
+          def initialize(host_env: ENV, program_name: $PROGRAM_NAME, socket: Socket)
+            @host_env = host_env
+            @program_name = program_name
+            @socket = socket
+          end
+
+          def call(telemetry)
             telemetry.transform_keys { |k| "sample##{k}" }.tap do |data|
-              data['name'] = 'Puma::Plugin::Telemetry'
-              # data['source'] = ??
+              data['name'] ||= 'Puma::Plugin::Telemetry'
+              data['source'] ||= source
             end
+          end
+
+          private
+
+          attr_reader :host_env, :program_name, :socket
+
+          def source
+            @source ||= host_env['L2MET_SOURCE'] ||
+                        host_env['DYNO'] || # For Heroku
+                        host_with_exe_name # Last-ditch effort
+          end
+
+          def host_with_exe_name
+            "#{socket.gethostname}/#{Pathname(program_name).basename}"
           end
         end
       end
