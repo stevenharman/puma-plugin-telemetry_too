babbel/puma-plugin-telemetry

Add Logfmt formatter
This is also the default `formatter:` of the new LogTarget.

lib/puma/plugin/telemetry/targets/base_formatting_target.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../formatters/json_formatter'
+require_relative '../formatters/logfmt_formatter'
 require_relative '../formatters/passthrough_formatter'
 require_relative '../transforms/cloud_watch_transform'
 require_relative '../transforms/passthrough_transform'
@@ -12,16 +13,8 @@ module Puma
         # A base class for other Targets concerned with formatting telemetry
         class BaseFormattingTarget
           def initialize(formatter: :json, transform: :cloud_watch)
-            @transform = case transform
-                         when :cloud_watch then Transforms::CloudWatchTranform
-                         when :passthrough then Transforms::PassthroughTransform
-                         else transform
-                         end
-            @formatter = case formatter
-                         when :json then Formatters::JSONFormatter
-                         when :passthrough then Formatters::PassthroughFormatter
-                         else formatter
-                         end
+            @formatter = FORMATTERS.fetch(formatter) { formatter }
+            @transform = TRANSFORMS.fetch(transform) { transform }
           end
 
           def call(_telemetry)
@@ -31,6 +24,19 @@ module Puma
           private
 
           attr_reader :formatter, :transform
+
+          FORMATTERS = {
+            json: Formatters::JSONFormatter,
+            logfmt: Formatters::LogfmtFormatter,
+            passthrough: Formatters::PassthroughFormatter
+          }.freeze
+          private_constant :FORMATTERS
+
+          TRANSFORMS = {
+            cloud_watch: Transforms::CloudWatchTranform,
+            passthrough: Transforms::PassthroughTransform
+          }.freeze
+          private_constant :TRANSFORMS
         end
       end
     end

stevenharman/puma-plugin-telemetry_too

lib/puma/plugin/telemetry/targets/base_formatting_target.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../formatters/json_formatter'
+require_relative '../formatters/logfmt_formatter'
 require_relative '../formatters/passthrough_formatter'
 require_relative '../transforms/cloud_watch_transform'
 require_relative '../transforms/passthrough_transform'
@@ -12,16 +13,8 @@ module Puma
         # A base class for other Targets concerned with formatting telemetry
         class BaseFormattingTarget
           def initialize(formatter: :json, transform: :cloud_watch)
-            @transform = case transform
-                         when :cloud_watch then Transforms::CloudWatchTransform
-                         when :passthrough then Transforms::PassthroughTransform
-                         else transform
-                         end
-            @formatter = case formatter
-                         when :json then Formatters::JSONFormatter
-                         when :passthrough then Formatters::PassthroughFormatter
-                         else formatter
-                         end
+            @formatter = FORMATTERS.fetch(formatter) { formatter }
+            @transform = TRANSFORMS.fetch(transform) { transform }
           end
 
           def call(_telemetry)
@@ -31,6 +24,19 @@ module Puma
           private
 
           attr_reader :formatter, :transform
+
+          FORMATTERS = {
+            json: Formatters::JSONFormatter,
+            logfmt: Formatters::LogfmtFormatter,
+            passthrough: Formatters::PassthroughFormatter
+          }.freeze
+          private_constant :FORMATTERS
+
+          TRANSFORMS = {
+            cloud_watch: Transforms::CloudWatchTransform,
+            passthrough: Transforms::PassthroughTransform
+          }.freeze
+          private_constant :TRANSFORMS
         end
       end
     end